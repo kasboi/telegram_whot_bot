d7b80cc05309bce1e6c9d7215d7c0f83326a4c96
Abdullah <solahudeen15@gmail.com>
2025-08-01 16:16:19 +0100

    removed useless scripts

--- scripts/check-creator.ts
+++
@@ -1,8 +0,0 @@
-const kv = await Deno.openKv()
-const result = await kv.get(["games", -1002665696422])
-const game = result.value as any
-
-console.log(`Creator ID: ${game.creatorId}`)
-console.log("Players:")
-game.players.forEach((p: any) => console.log(`- ${p.firstName} (${p.id}) ${p.id === game.creatorId ? "👑 CREATOR" : ""}`))
-await kv.close()
